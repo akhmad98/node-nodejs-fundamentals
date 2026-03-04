25d84e93db03b1ddd713a0ad988a3fe5315a7a9e
Akhmad Badalov <akhmadbadalov98@gmail.com>
2026-03-05 02:27:24 +0500

    feat: exec cmd

--- src/cp/execCommand.js
+++ src/cp/execCommand.js
@@ -1,10 +1,41 @@
+import { spawn } from 'child_process';
+
 const execCommand = () => {
-  // Write your code here
-  // Take command from CLI argument
-  // Spawn child process
-  // Pipe child stdout/stderr to parent stdout/stderr
-  // Pass environment variables
-  // Exit with same code as child
+  const arg = process.argv.slice(2);
+
+  const cmdAndArgs = arg[0].split(" ");
+  console.log(cmdAndArgs, 'sdsd')
+  const [childCMD, ...childCMDComment] = cmdAndArgs;
+  let cmd;
+
+  console.log(childCMDComment);
+  if (childCMDComment.length) {
+    cmd = spawn(childCMD, [...childCMDComment], {
+      stdio: 'inherit',
+      env: process.env,
+    });
+  }
+
+  cmd = spawn(childCMD);
+  
+  cmd.stdout.on('data', (data) => {
+    process.stdout.write(`${data} from child`);
+  });
+
+  cmd.stderr.on('data', (data) => {
+    console.log(data, 'p')
+    process.stderr.write(`Error look: ${data}`);
+  });
+
+  cmd.on('exit', (code, signal) => {
+    if (code !== null) {
+      process.exit(code);
+    } else {
+      cmd.close('close', (code) => {
+        process.exit(code);
+      })
+    }
+  });
 };
 
 execCommand();
