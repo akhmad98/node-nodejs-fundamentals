07988e7e2001871eeb170b28d49af2bc67974ad0
Akhmad Badalov <akhmadbadalov98@gmail.com>
2026-03-04 17:22:42 +0500

    feat: cli first task

--- src/cli/interactive.js
+++ src/cli/interactive.js
@@ -1,8 +1,42 @@
+import { createInterface } from 'node:readline/promises';
+import { stdin, stdout } from 'node:process';
+
 const interactive = () => {
-  // Write your code here
-  // Use readline module for interactive CLI
-  // Support commands: uptime, cwd, date, exit
-  // Handle Ctrl+C and unknown commands
+  const rl = createInterface({ 
+    input: stdin, 
+    output: stdout,
+  });
+
+  rl.setPrompt('> ');
+  rl.prompt();
+
+  rl.on('line', (cmd) => {
+    switch(cmd) {
+      case 'uptime':
+        const processUptime = process.uptime();
+        console.log(`Uptime: ${processUptime}`);
+        break;
+      case 'cwd':
+        const currDir = process.cwd();
+        console.log(currDir.split('/')[currDir.split('/').length-1]);
+        break;
+      case 'date':
+        const currentDate = new Date();
+        const ISOstring = currentDate.toISOString();
+        console.log(ISOstring);
+        break;
+      case 'exit':
+        console.log('Goodbye!');
+        process.exit(0);
+      default:
+        console.log('Unknown command');
+    }
+  })
+
+  rl.on('SIGINT', () => {
+    console.log('Goodbye!');
+    rl.close();
+  });
 };
 
 interactive();
