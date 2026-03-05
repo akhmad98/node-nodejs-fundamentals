70370295b74b21332605242e2265aa600d59ea2c
Akhmad Badalov <akhmadbadalov98@gmail.com>
2026-03-05 20:56:05 +0500

    feat: module dynamic

--- src/modules/dynamic.js
+++ src/modules/dynamic.js
@@ -1,9 +1,44 @@
+import fs from 'node:fs/promises';
+
+const plugins = ['uppercase', 'reverse', 'repeat'];
+
 const dynamic = async () => {
-  // Write your code here
-  // Accept plugin name as CLI argument
-  // Dynamically import plugin from plugins/ directory
-  // Call run() function and print result
-  // Handle missing plugin case
+  const dir = process.cwd() + '/src/modules';
+  const argv = process.argv.slice(2);
+  if (!plugins.includes(argv[0])) {
+    console.error('Plugin not found!');
+    process.exit(1);
+  }
+  let module;
+  let result;
+
+  try {
+    await fs.access(`${dir}/plugins`);
+    const arrModulePromises = plugins.map(async (el) => {
+      await fs.access(`${dir}/plugins/${el}.js`);
+      return `./plugins/${el}.js`;
+    });
+
+    const arrModules = await Promise.all(arrModulePromises);
+    
+    for (const mod of arrModules) {
+      if (mod === `./plugins/${argv[0]}.js`) {
+        module = await import(mod);
+        break;
+      }
+    }
+    result = module.run();
+  } catch (error) {
+    if (error === 'ENOENT') {
+      console.error('Error: ', "No such file exist!");
+    } else if (error === 'EACCES') {
+      console.error('Error: ', "Access Denied!")
+    } else {
+      console.error('Error: ', error)
+    }
+  } finally {
+    process.stdin.write(result);
+  }
 };
 
 await dynamic();
